034c76a8f860bd04a65356068b5c3e13ae6c299f
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-07-28 13:13:02 -0700

    added a few variables

--- data.py
+++ data.py
@@ -1,6 +1,17 @@
 import streamlit as sl
 import pandas as pd
+import sportsipy
 
 """
-    # Explore the NBA's most recent stat lines
+    # Check out about 70 years worth of NBA statistics!
 """
+year_list = list(range(2010, 2019))
+
+year_select = sl.sidebar.selectbox('Season Starting Year', year_list)
+
+positions_array = ['PG', 'SG', 'SF', 'PF', 'C']
+
+select_array = sl.sidebar.multiselect('Positions', positions_array, positions_array)
+
+print(select_array)
+print(year_select)
