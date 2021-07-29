7a2dc35d7947cc8385f60278741f11830386889d
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-07-29 16:14:15 -0700

    added data and soem features to sidebar

--- data.py
+++ data.py
@@ -1,13 +1,25 @@
 import streamlit as sl
 import pandas as pd
-import sportsipy
 
 """
-    # Check out about 70 years worth of NBA statistics!
+    # Check out about 70 years worth of statistics around the NBA!
 """
-year_list = list(range(2010, 2019))
 
-year_select = sl.sidebar.selectbox('Season Starting Year', year_list)
+
+def load_data():
+    data = pd.read_html("https://www.basketball-reference.com/leagues/NBA_2021_per_game.html", header = 0)
+    print(data)
+
+data = pd.read_html("https://www.basketball-reference.com/leagues/NBA_2021_per_game.html", header=0)
+sl.dataframe(data[0])
+
+year_list = list(range(1950, 2021))
+
+sl.sidebar.header('Data Filters')
+
+year_select = sl.sidebar.selectbox("Select the starting year of the season you'd like to explore:", year_list)
+
+# team_select = sl.sidebar.multiselect('Teams', )
 
 positions_array = ['PG', 'SG', 'SF', 'PF', 'C']
 
