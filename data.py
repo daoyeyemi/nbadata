0efba6a1f34163bac1515f8119454e7b1f0101f2
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-07-30 11:48:10 -0700

    added filters and deleted unnecessary rows and columns

--- data.py
+++ data.py
@@ -6,19 +6,22 @@ import numpy as np
     # Check out about 70 years worth of statistics around the NBA!
 """
 
-# def load_data():
-#     data = pd.read_html("https://www.basketball-reference.com/leagues/NBA_" + {} + "_per_game.html", header = 0)
-#     print(data)
 year_list = list(range(1950, 2021))
 
-year_select = sl.sidebar.selectbox("Select the starting year of the season you'd like to explore:", year_list)
+sl.sidebar.header('Data Filters')
 
+year_select = sl.sidebar.selectbox("Select the starting year of the season you'd like to explore:", year_list, key="a")
 
 data = pd.read_html("https://www.basketball-reference.com/leagues/NBA_" + str(year_select) + "_per_game.html", header=0)
+data = data[0].drop(['Rk'], axis=1)
+# got rid of repeating headers in dataframe
+data = data[data['Player'] != 'Player']
+# filtering data that has the TOT for the team
+data = data[data['Tm'] != 'TOT']
 
-sl.dataframe(data[0])
+sl.dataframe(data)
 # showing values from team column
-teams_col = data[0].loc[:, "Tm"]
+teams_col = data.loc[:, "Tm"]
 
 print(teams_col)
 # showing values from team column
@@ -29,15 +32,13 @@ print(teams_list)
 
 # print(team_values.unique())
 
-sl.sidebar.header('Data Filters')
-
-year_select = sl.sidebar.selectbox("Select the starting year of the season you'd like to explore:", year_list, key="a")
-
-team_select = sl.sidebar.multiselect('Teams', teams_list, teams_list)
+teams_selected = sl.sidebar.multiselect('Teams', teams_list, teams_list)
 
 positions_array = ['PG', 'SG', 'SF', 'PF', 'C']
 
-select_array = sl.sidebar.multiselect('Positions', positions_array, positions_array)
+positions_selected = sl.sidebar.multiselect('Positions', positions_array, positions_array)
 
-print(select_array)
-print(year_select)
+req1 = data["Tm"].isin(teams_selected)
+req2 = data["Pos"].isin(positions_selected)
+
+data = data[req1 & req2]
