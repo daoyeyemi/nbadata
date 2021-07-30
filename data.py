a350e812eae2d0522ed44b106f66cc4f2fc17471
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-07-29 21:41:59 -0700

    added year functionality wideget that actually works

--- data.py
+++ data.py
@@ -1,25 +1,39 @@
 import streamlit as sl
 import pandas as pd
+import numpy as np
 
 """
     # Check out about 70 years worth of statistics around the NBA!
 """
 
-
-def load_data():
-    data = pd.read_html("https://www.basketball-reference.com/leagues/NBA_2021_per_game.html", header = 0)
-    print(data)
-
-data = pd.read_html("https://www.basketball-reference.com/leagues/NBA_2021_per_game.html", header=0)
-sl.dataframe(data[0])
-
+# def load_data():
+#     data = pd.read_html("https://www.basketball-reference.com/leagues/NBA_" + {} + "_per_game.html", header = 0)
+#     print(data)
 year_list = list(range(1950, 2021))
-
-sl.sidebar.header('Data Filters')
 
 year_select = sl.sidebar.selectbox("Select the starting year of the season you'd like to explore:", year_list)
 
-# team_select = sl.sidebar.multiselect('Teams', )
+
+data = pd.read_html("https://www.basketball-reference.com/leagues/NBA_" + str(year_select) + "_per_game.html", header=0)
+
+sl.dataframe(data[0])
+# showing values from team column
+teams_col = data[0].loc[:, "Tm"]
+
+print(teams_col)
+# showing values from team column
+# team_values = np.array(teams_col.values)
+# finally got unique array
+teams_list = pd.unique(teams_col)
+print(teams_list)
+
+# print(team_values.unique())
+
+sl.sidebar.header('Data Filters')
+
+year_select = sl.sidebar.selectbox("Select the starting year of the season you'd like to explore:", year_list, key="a")
+
+team_select = sl.sidebar.multiselect('Teams', teams_list, teams_list)
 
 positions_array = ['PG', 'SG', 'SF', 'PF', 'C']
 
